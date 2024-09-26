d3323b33ceedb6337a3156212c803fa65b0dcfaf
NikolasSucadorni <nikolassucadorni@gmail.com>
2024-09-26 12:01:39 -0300

    update resolution

--- dash.py
+++ dash.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(layout="wide")
+
 df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
 df["Date"] = pd.to_datetime(df["Date"])
 df=df.sort_values("Date")
